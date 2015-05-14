bbc399794389ba01d9ad6c42f49b5dbc81b4f6ec
Taisuke Miyazaki <onepiece.love0314@gmail.com>
2015-05-15 02:45:50 +0900

    configure markdown options

--- src/js/marked.js
+++ src/js/marked.js
@@ -1,3 +1,19 @@
 var marked = require('marked');
+var renderer = new marked.Renderer();
+
+renderer.link = function(href, title, text) {
+    return '<a href="' + href + '" target="_blank">' + text + '</a>';
+}
+
+marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+});
 
 module.exports = marked;
