3fad52c32c1c270145956a3033a90c5321ffad7c
Taisuke Miyazaki <onepiece.love0314@gmail.com>
2015-05-15 03:24:02 +0900

    Add many colorschemes.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 var Vue = require('vue');
 var marked = require('./marked.js');
+var langFiles = require('./lang.js');
 
 var app = new Vue({
     el: '#markdown',
     data: {
-        text: ''
+        text: '',
+        langFiles: langFiles(),
+        langFile: 'default.css'
     },
     methods: {
         replaceTab: function(e) {
@@ -15,6 +18,11 @@ var app = new Vue({
             value = elem.value;
             elem.value = "" + (value.substring(0, start)) + "\t" + (value.substring(end));
             elem.selectionStart = elem.selectionEnd = start + 1;
+        }
+    },
+    components: {
+        'lazy-link': {
+            template: '<link rel="stylesheet" href="css/lang/{{file}}">'
         }
     },
     filters: {
