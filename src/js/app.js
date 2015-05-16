53c63c84e788dc03956324144a83e222791bb52a
Taisuke Miyazaki <imishinist@gmail.com>
2015-05-16 19:41:21 +0900

    Select tab size.

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,15 @@ var app = new Vue({
     el: '#markdown',
     data: {
         text: '',
+        tabsize: "4",
+        tabs: ["2", "4", "8"],
         langFiles: langFiles(),
         langFile: localStorage.getItem('langFile') || 'default.css'
     },
     methods: {
+        changeTabsize: function(e) {
+            editor.getSession().setTabSize(this.tabsize);
+        },
         storeLang: function(e) {
             localStorage.setItem("langFile", this.langFile);
         }
@@ -27,3 +32,4 @@ var app = new Vue({
 editor.on('change', function() {
     app.text = editor.getSession().getValue();
 });
+
