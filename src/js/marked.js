a0870f21ee4c4c5b5bdbfd36b7c460659b9c59aa
Taisuke Miyazaki <imishinist@gmail.com>
2015-05-16 19:23:07 +0900

    Some code is into local scope.

--- src/js/marked.js
+++ src/js/marked.js
@@ -1,25 +1,26 @@
-var marked = require('marked');
-var hl = require('highlight.js');
-var renderer = new marked.Renderer();
+module.exports = function() {
+    var marked = require('marked');
+    var hl = require('highlight.js');
+    var renderer = new marked.Renderer();
 
-hl.initHighlightingOnLoad();
-renderer.link = function(href, title, text) {
-    return '<a href="' + href + '" target="_blank">' + text + '</a>';
-}
-
-marked.setOptions({
-    renderer: renderer,
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false,
-    langPrefix: '',
-    highlight: function(code) {
-        return hl.highlightAuto(code).value;
+    hl.initHighlightingOnLoad();
+    renderer.link = function(href, title, text) {
+        return '<a href="' + href + '" target="_blank">' + text + '</a>';
     }
-});
 
-module.exports = marked;
+    marked.setOptions({
+        renderer: renderer,
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false,
+        langPrefix: '',
+        highlight: function(code) {
+            return hl.highlightAuto(code).value;
+        }
+    });
+    return marked;
+}();
